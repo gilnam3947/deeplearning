2d88a69ca3cd2793c408b6a6e1a3a09db5548d35
이길남 <115065890+gilnam3947@users.noreply.github.com>
2022-10-05 16:18:02 +0900

    Add files via upload

---
+++ 수열의 합.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 25 00:35:10 2022
+
+@author: HappyDay
+"""
+# 시그마
+import numpy as np
+
+a = np.array([1, 2, 3, 4, 5])    # a1 ~ a5의 수를 배열로 선언
+y = np.sum(a)         # 수열의 합
+
+print(y)
